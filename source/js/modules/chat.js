77a47542a2daed53d3be286c385b43d67d4f38bd
Kirill Samsonov <kvsamsonov@avito.ru>
2020-09-10 19:47:36 +0300

    reverse

--- source/js/modules/chat.js
+++ source/js/modules/chat.js
@@ -15,7 +15,6 @@ export default () => {
 
     let getAnswer = function () {
       setTimeout(function () {
-
         let answerEl = document.createElement(`li`);
         let placeholder = document.createElement(`div`);
         let textEl = document.createElement(`p`);
